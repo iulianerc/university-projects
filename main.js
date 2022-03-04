b369822fcb92198b3104f7bd2bdb8eb33b557988
iulianerc <colesnic.2002@mail.ru>
2022-03-04 12:45:51 +0200

    1 - Finished

---
+++ main.js
@@ -0,0 +1,12 @@
+
+window.addEventListener('load', (event) => {
+  document.body.style.color = 'yellow'
+  document.body.style.fontSize = 30
+  document.body.innerHTML = `
+    <div>Let's do it</div>
+  `
+})
+
+console.log(
+  'Mh'
+)
