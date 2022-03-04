57f95dfebaf6f142cb0dbbb888e6518f628de3dc
iulianerc <colesnic.2002@mail.ru>
2022-03-04 12:54:41 +0200

    Task-1

---
+++ Task-1/main.js
@@ -0,0 +1,7 @@
+window.addEventListener('load', (event) => {
+  document.body.style.color = 'aqua'
+  document.body.style.fontSize = '30px'
+  document.body.innerHTML = `
+    <div>Let's do it</div>
+  `
+})
