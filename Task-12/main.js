07f7f8edf88923a68ca0c685871cae93f3e44a95
iulianerc <colesnic.2002@mail.ru>
2022-03-04 14:29:41 +0200

    Task - 12

---
+++ Task-12/main.js
@@ -0,0 +1,15 @@
+const ol = document.getElementById('ol')
+
+const numbers = Array.apply(null, {length: 100}).map(() => Math.floor(Math.random() * 100))
+
+numbers.forEach((number, index) => {
+  setTimeout(() => {
+    const li = document.createElement('li')
+    li.innerText = `----------- ${number}`
+    
+    ol.appendChild(li)
+    console.log(index)
+  }, index * 1000)
+  
+})
+ 
