0bb5511d9f555c2acb75f072127cebaab2ff7810
iulianerc <colesnic.2002@mail.ru>
2022-04-16 18:40:57 +0300

    Init vue

--- vuejs-app/src/App.vue
+++ vuejs-app/src/App.vue
@@ -1,28 +1,36 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <div class="d-flex align-center">
+        <span class="font-weight-bold" style="font-size: 20px;">Iulianerc</span>
+      </div>
+      
+      <v-spacer></v-spacer>
+      
+<!--      <v-icon size="40">mdi-menu</v-icon>-->
+    </v-app-bar>
+    <v-main>
+      <app-body></app-body>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AppBody from "@/components/AppBody/AppBody";
 
 export default {
   name: 'App',
+  
   components: {
-    HelloWorld
-  }
-}
+    AppBody
+  },
+  
+  data: () => ({
+    //
+  }),
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ vuejs-app/src/components/AppBody/AppBody.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="AppBody">
+        <h1>Go!</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'AppBody'
+    };
+</script>
